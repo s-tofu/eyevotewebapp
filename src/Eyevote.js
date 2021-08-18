ac6b087a7a06863f979e8b29e03256da6da55d8c
s-tofu <khanhphuong@outlook.de>
2021-08-18 19:59:17 +0200

    Log answerOne coordinates

--- src/Eyevote.js
+++ src/Eyevote.js
@@ -3,6 +3,7 @@ import './Eyevote.css'
 
 
 const EyeVote = () => {
+    const[question, setQuestion] = useState('0')
     
     function start() {
         window.GazeCloudAPI.StartEyeTracking()
@@ -10,18 +11,29 @@ const EyeVote = () => {
     }
 
     function giveMeYourShit(result) {
-        console.log(result)
         document.getElementById('dotLookAt').style.left =`${result.docX}px`;
         document.getElementById('dotLookAt').style.top =`${result.docY}px`;
+        if (document.getElementById('answerOne').style.left == `${result.docX}px` && document.getElementById('answerOne').style.top == `${result.docY}px`) {
+            console.log("Answer one selected!")
+        }
+    }
+
+    function performMovement() {
+        console.log(document.getElementById('answerOne').style.top)
     }
     
     return (
             <div className='Eyevote'>
+            performMovement()
              <h1 className='header'>EyeVote Remote</h1>
              <p className='instructions'>You will be presented 10 questions</p>
              <p className='instructions'>Please gaze at the answers you want to select</p>
-             <button className="button" onClick={() => start()}>Start eye-tracking</button>
-             <div className="dot" id="dotLookAt"></div>
+             <button className='button' onClick={() => start()}>Start eye tracking</button>
+             <div className='dot' id='dotLookAt'></div>
+             <h1 className='question'>What is your favorite ice cream?</h1>
+             <h1 className='answer' id="answerOne">Vanille</h1>
+             <h1 className='answer' id="answerTwo">Chocolate</h1>
+             <h1 className='answer'>Strawberry</h1>
             </div>
     )
 }
