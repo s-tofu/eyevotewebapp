085f5b4293afcfe1c324833c158414695adaa1d7
Khanh-H <58639731+s-tofu@users.noreply.github.com>
2021-08-18 13:42:59 +0200

    Working Script

--- src/Eyevote.js
+++ src/Eyevote.js
@@ -1,26 +1,27 @@
-import React from 'react'
-import useScript from './useScript'
+import React, { useState, useEffect } from 'react'
 import './Eyevote.css'
 
 
-
 const EyeVote = () => {
-    const startTracking = () => {
-    window.GazeCloudAPI.StartEyeTracking()
-     }
-    const stopTracking = () => {
-    window.GazeCloudAPI.StopEyeTracking()
+    
+    function start() {
+        window.GazeCloudAPI.StartEyeTracking()
+        window.GazeCloudAPI.OnResult = giveMeYourShit
     }
 
+    function giveMeYourShit(result) {
+        console.log(result)
+    }
+    
     return (
-        <div className='Eyevote'>
-            <p className='Eyevote'>
-            </p>
-            <h1 className='header'>EyeVote Remote</h1>
-            <p className='instructions'>You will be presented 10 questions</p>
-            <p className='instructions'>Please gaze at the answers you want to select</p>
-            <button className="button" onClick={startTracking}>Start eye-tracking</button>
-        </div>
+            <div className='Eyevote'>
+            <p className='Eyevote'>Hello World</p>
+             <h1 className='header'>EyeVote Remote</h1>
+             <p className='instructions'>You will be presented 10 questions</p>
+             <p className='instructions'>Please gaze at the answers you want to select</p>
+             <button className="button" onClick={() => start()}>Start eye-tracking</button>
+             <div className="dot" id="dotLookAt"></div>
+            </div>
     )
 }
 
