2df1db4047b76572a62f26b81379c7cc17712863
Khanh-H <58639731+s-tofu@users.noreply.github.com>
2021-08-18 14:11:31 +0200

    Added Home component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react'
 import Header from './Header';
-import useScript from './useScript'
 import Consent from './Consent';
 import Questions from './Questions';
+import Home from './Home'
 import Study from './Eyevote.js';
 import './App.css'
 
@@ -18,6 +18,8 @@ function App() {
       return <Consent />
     } else if (page === 'questions') {
       return <Questions />
+    } else if (page === 'home') {
+      return <Home />
     }
   }
 
@@ -26,21 +28,6 @@ function App() {
   }
   return (
     <div className="App">
-      <Header></Header>
-      <h1 className="title">Remote Eye-Tracking Study: EyeVote
-      </h1>
-      <p className="body">First of all, thank you for your interest in my Bachelor Thesis study “Remote Eye-Tracking Studies: challenges and opportunities of conducting eye-tracking studies out of the lab”. 
-      </p>
-      <p className="body">For this study, please use your desktop with a webcam or your laptop. A webcam is needed to track your eyes. Once the study starts, you will be asked 10 questions and will gaze at the moving answers that you want to select. The questions will only include neutral and personal questions where there is
-        no right or wrong. E.g. “Which ice cream flavor would you choose? Vanilla, Chocolate or Strawberry.”
-      </p>
-      <p className="body">This study will take approximately 15 minutes.
-      </p>
-      <p className="body">After clicking Start, you will be led to the Consent Form first. We would then like to ask you to answer some demographic questions.
-      </p>
-      <button className="start-button"><a href="" onClick={toPage('consent')} style={padding}>
-          Start
-        </a></button>
        {content()}
     </div>
   );
