0c68879472aab6e04e41ee1971cd781a19ebddff
Khanh-H <58639731+s-tofu@users.noreply.github.com>
2021-08-19 03:18:30 +0200

    Added

--- src/Eyevote.js
+++ src/Eyevote.js
@@ -2,25 +2,31 @@ import React, { useState, useEffect } from 'react'
 import './Eyevote.css'
 
 const EyeVote = () => {
+    // State to show Question, shows StartScreen on State zero
     const[question, setQuestion] = useState('0')
 
+    // Question prompts
+
+    // Conditional Question State control
     const questionNumber = () => {
         if (question === '0') {
           return(StartScreen());
-        } else {
+        } else if(question === '1'){
             return(QuestionScreen());
         }
       }
     
+    // Function on clicking Start button
     function start() {
         // Start with the Calobration and start Eyetracker
-        window.GazeCloudAPI.StartEyeTracking()
+        // window.GazeCloudAPI.StartEyeTracking()
         
         performMovement()
         // Use Gaze 
-        window.GazeCloudAPI.OnResult = PlotGaze
+        //window.GazeCloudAPI.OnResult = PlotGaze
     }
 
+    // Handle Gaze results
     function PlotGaze(result) {
         document.getElementById('dotLookAt').style.left =`${result.docX}px`;
         document.getElementById('dotLookAt').style.top =`${result.docY}px`;
@@ -30,8 +36,11 @@ const EyeVote = () => {
     }
 
     function performMovement() {
+        console.log(document.getElementById('answerOne').style.top)
     }
     
+
+    // First screen 
     const StartScreen = () => {
         return (
             <div className='Eyevote'>
@@ -43,13 +52,14 @@ const EyeVote = () => {
         );
     }
 
+    // Question screen
     const QuestionScreen = () => {
         return (
             <div className='Eyevote'>
-                <h1 className='question'>What is your favorite ice cream?</h1>
-                <h1 className='answer' id="answerOne">Vanille</h1>
-                <h1 className='answer' id="answerTwo">Chocolate</h1>
-                <h1 className='answer'>Strawberry</h1>
+                <h1 className='question' id="questionPrompt">What is your favorite ice cream?</h1>
+                <h1 className='answerOne' id="answerOne">Vanille</h1>
+                <h1 className='answerTwo' id="answerTwo">Chocolate</h1>
+                <h1 className='answerThree' id="answerThree">Strawberry</h1>
             </div>
         );
     }
