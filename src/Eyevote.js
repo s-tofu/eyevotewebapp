0d6b3cf36712caae8a1f3af8496729028681300f
Khanh-H <58639731+s-tofu@users.noreply.github.com>
2021-08-21 19:09:44 +0200

    Added react-spring animation

--- src/Eyevote.js
+++ src/Eyevote.js
@@ -1,9 +1,28 @@
 import React, { useState, useEffect } from 'react'
+import { useSpring, animated } from 'react-spring'
 import './Eyevote.css'
 
 const EyeVote = () => {
     // State to show Question, shows StartScreen on State zero
     const[question, setQuestion] = useState('0')
+    const [flip, setFlip] = useState(false)
+
+    // x and y coordinates of element
+    //var answerOne_rect = document.getElementById('answerOne').getBoundingClientRect();
+    //var answerOne_x = answerOne_rect.left;
+    //var answerOne_y = answerOne_rect.top;
+
+    // Style of animation
+    const props = useSpring(
+        { 
+            to: { x: 1, y: 1 }, 
+            from: { x: 350, y: 350 }, 
+            reset: true,
+            reverse: flip,
+            config:{mass:1, tension:200, friction:200},
+            onRest: () => setFlip(!flip) 
+        })
+
 
     // Question prompts
 
@@ -19,24 +38,29 @@ const EyeVote = () => {
     // Function on clicking Start button
     function start() {
         // Start with the Calobration and start Eyetracker
-        // window.GazeCloudAPI.StartEyeTracking()
+        window.GazeCloudAPI.StartEyeTracking()
         
         performMovement()
         // Use Gaze 
-        //window.GazeCloudAPI.OnResult = PlotGaze
+        window.GazeCloudAPI.OnResult = PlotGaze
     }
 
     // Handle Gaze results
     function PlotGaze(result) {
         document.getElementById('dotLookAt').style.left =`${result.docX}px`;
         document.getElementById('dotLookAt').style.top =`${result.docY}px`;
-        if (document.getElementById('answerOne').style.left == `${result.docX}px` && document.getElementById('answerOne').style.top == `${result.docY}px`) {
+        // x and y coordinates of element
+        var answerOne_rect = document.getElementById('answerOne').getBoundingClientRect();
+        var answerOne_x = answerOne_rect.left;
+        var answerOne_y = answerOne_rect.top;
+        console.log("x: "+ answerOne_x + "y:" + answerOne_y)
+        console.log("gazex: " + result.docX +" gazey:"+ result.docY)
+        if (answerOne_y === `${result.docX}px` && answerOne_x === `${result.docY}px`) {
             console.log("Answer one selected!")
         }
     }
 
     function performMovement() {
-        console.log(document.getElementById('answerOne').style.top)
     }
     
 
@@ -45,8 +69,7 @@ const EyeVote = () => {
         return (
             <div className='Eyevote'>
                  <h1 className='header'>EyeVote Remote</h1>
-                 <p className='instructions'>You will be presented 10 questions</p>
-                 <p className='instructions'>Please gaze at the answers you want to select</p>
+                 <p className='instructions'>We will start with a calibration.<p></p>After calibration you will be presented 10 questions. <p></p>Please gaze at the answers you want to select.</p>
                  <button className='button' onClick={() => {start(); setQuestion('1');}}>Start eye tracking</button>
             </div>
         );
@@ -57,9 +80,9 @@ const EyeVote = () => {
         return (
             <div className='Eyevote'>
                 <h1 className='question' id="questionPrompt">What is your favorite ice cream?</h1>
-                <h1 className='answerOne' id="answerOne">Vanille</h1>
-                <h1 className='answerTwo' id="answerTwo">Chocolate</h1>
-                <h1 className='answerThree' id="answerThree">Strawberry</h1>
+                <animated.div style={props}><label className='answerOne' id="answerOne">Vanille</label></animated.div>
+                <label className='answerTwo' id="answerTwo">Chocolate</label>
+                <label className='answerThree' id="answerThree">Strawberry</label>
             </div>
         );
     }
