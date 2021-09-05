a5568ed74e0e44d96b1291a02ca5ec907cd5b51b
Khanh-H <58639731+s-tofu@users.noreply.github.com>
2021-09-05 13:29:58 +0200

    Put Label Positions into arrays

--- src/Eyevote.js
+++ src/Eyevote.js
@@ -1,16 +1,38 @@
 import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom';
 import { useSpring, animated } from 'react-spring'
 import './Eyevote.css'
 
 const EyeVote = () => {
+    // Attributes
+
     // State to show Question, shows StartScreen on State zero
-    const[question, setQuestion] = useState('0')
+    const[question, setQuestion] = useState('1')
     const [flip, setFlip] = useState(false)
 
-    // x and y coordinates of element
-    //var answerOne_rect = document.getElementById('answerOne').getBoundingClientRect();
-    //var answerOne_x = answerOne_rect.left;
-    //var answerOne_y = answerOne_rect.top;
+    // This attribute is set to true if an answer was selected
+    var answerOne = false;
+    var answerTwo = false;
+    var answerThree = false;
+
+    // Labels
+    // x and y coordinates of labels
+    var answerOne_x
+    var answerOne_y
+    var answerTwo_x
+    var answerTwo_y
+    var answerThree_x
+    var answerThree_y
+
+    // correlations of labels
+    var corAnswerOne
+    var corAnswerTwo
+    var corAnswerThree
+
+    //On Load 
+    var presentUser = true;
+    var logLabelPosition = [];
+    var logGazePosition = [];
 
     // Style of animation
     const props = useSpring(
@@ -19,7 +41,7 @@ const EyeVote = () => {
             from: { x: 350, y: 350 }, 
             reset: true,
             reverse: flip,
-            config:{mass:1, tension:200, friction:200},
+            //config:{mass:1, tension:200, friction:200},
             onRest: () => setFlip(!flip) 
         })
 
@@ -38,21 +60,18 @@ const EyeVote = () => {
     // Function on clicking Start button
     function start() {
         // Start with the Calobration and start Eyetracker
-        window.GazeCloudAPI.StartEyeTracking()
+        //window.GazeCloudAPI.StartEyeTracking()
         
-        performMovement()
+        //performMovement()
         // Use Gaze 
-        window.GazeCloudAPI.OnResult = PlotGaze
+        //window.GazeCloudAPI.OnResult = PlotGaze
     }
 
     // Handle Gaze results
     function PlotGaze(result) {
         document.getElementById('dotLookAt').style.left =`${result.docX}px`;
         document.getElementById('dotLookAt').style.top =`${result.docY}px`;
-        // x and y coordinates of element
-        var answerOne_rect = document.getElementById('answerOne').getBoundingClientRect();
-        var answerOne_x = answerOne_rect.left;
-        var answerOne_y = answerOne_rect.top;
+
         console.log("x: "+ answerOne_x + "y:" + answerOne_y)
         console.log("gazex: " + result.docX +" gazey:"+ result.docY)
         if (answerOne_y === `${result.docX}px` && answerOne_x === `${result.docY}px`) {
@@ -62,6 +81,75 @@ const EyeVote = () => {
 
     function performMovement() {
     }
+
+    // calculates Correlation
+    function calculateCorrelation() {
+        // import the fn for correlation
+        const calculateCorrelation = require("calculate-correlation");
+
+        // given 4 points: (2,3), (5,3), (4,6) and (1,7)    
+        const x = [2, 5, 4, 1];
+        const y = [3, 3, 6, 7];
+
+        // get the x and y coordinates of the labels and assign them
+        var answerOne_rect = document.getElementById('answerOne').getBoundingClientRect();
+        var answerTwo_rect = document.getElementById('answerTwo').getBoundingClientRect();
+        var answerThree_rect = document.getElementById('answerThree').getBoundingClientRect();
+        answerOne_x = answerOne_rect.left;
+        answerOne_y = answerOne_rect.top;
+        answerTwo_x = answerTwo_rect.left;
+        answerTwo_y = answerTwo_rect.top;
+        answerThree_x = answerThree_rect.left;
+        answerThree_y = answerThree_rect.top;
+
+        // console log the answers 
+        console.log("AnswerOne x: "+ answerOne_x + " y:" + answerOne_y)
+        console.log("AnswerTwo x: "+ answerTwo_x + " y:" + answerTwo_y)
+        console.log("AnswerThree x: "+ answerThree_x + " y:" + answerThree_y)
+
+        // constantly push the positions into the position arrays after each second
+        const interval = setInterval(() => {
+            logLabelPosition.push(answerOne_x);
+            logLabelPosition.push(answerOne_y);
+          }, 1000);
+        console.log(logLabelPosition);
+
+        // calculate the correlation
+        const correlation = calculateCorrelation(x, y);
+
+        if ((answerOne === false) && (answerTwo === false) && (answerThree === false))
+                    {
+                        // If corelation for answer one is over corReference
+                        if (((corAnswerOne) >= 1.4))
+                        {
+                            console.log("Chosen: Answer One");
+                            answerOne = true;
+                        }
+
+                        // If corelation for answer one is over corReference
+                        else if (((corAnswerTwo) >= 1.4))
+                        {
+                            console.log("Chosen: Answer Two");
+                            answerTwo = true;
+                        }
+
+                        // If corelation for answer one is over corReference
+                        else if (((corAnswerThree) >= 1.4))
+                        {
+                            console.log("Chosen: Answer Three");
+                            answerThree = true;
+                        }
+                    }
+                    else
+                    {
+                        answerOne = false;
+                        answerTwo = false;
+                        answerThree = false;
+                    }
+
+        console.log(correlation); // logs -0.442807443
+        console.log(typeof correlation); // logs number
+    }
     
 
     // First screen 
@@ -69,6 +157,7 @@ const EyeVote = () => {
         return (
             <div className='Eyevote'>
                  <h1 className='header'>EyeVote Remote</h1>
+                 <animated.div style={props}><label className='answerOne' id="answerOne">Vanille</label></animated.div>
                  <p className='instructions'>We will start with a calibration.<p></p>After calibration you will be presented 10 questions. <p></p>Please gaze at the answers you want to select.</p>
                  <button className='button' onClick={() => {start(); setQuestion('1');}}>Start eye tracking</button>
             </div>
@@ -77,6 +166,12 @@ const EyeVote = () => {
 
     // Question screen
     const QuestionScreen = () => {
+        useEffect(() => {
+            const interval = setInterval(() => {
+                performMovement();
+                calculateCorrelation();
+              }, 10000);
+            }, [])
         return (
             <div className='Eyevote'>
                 <h1 className='question' id="questionPrompt">What is your favorite ice cream?</h1>
